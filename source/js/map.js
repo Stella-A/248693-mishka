6fa42f10739895d055fc790f79a9eaab488f9f2b
Stella-A <abbasovt_93@mail.ru>
2018-01-29 19:03:46 +0500

    Оформление страниц с помощью препроцессора

--- source/js/map.js
+++ source/js/map.js
@@ -3,12 +3,13 @@
 function initMap() {
   var coords = {lat: 59.938734, lng: 30.323121};
   var map = new google.maps.Map(document.getElementById('map'), {
+    disableDefaultUI: true,
     zoom: 17,
     center: coords
   });
   var marker = new google.maps.Marker({
     position: coords,
     map: map,
-    icon: 'img/marker.svg'
+    icon: 'img/icon-map-pin.svg'
   });
 }
